3b130c7e2c9c01f14c00d7f32dfdf657d5724ab9
Nandha kuame <nandhakumarmani22@gmail.com>
2026-04-10 14:54:52 +0530

    agsi change

--- chat/asgi.py
+++ chat/asgi.py
@@ -1,14 +1,21 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import chatapp.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
+
+# This must come BEFORE importing anything that uses Django models/apps
+django.setup()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing  # or wherever your websocket urlpatterns are
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(chatapp.routing.websocket_urlpatterns)
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
     ),
 })
--- chat/settings.py
+++ chat/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'channels',
+    'daphne',
     'cloudinary',
     'cloudinary_storage',
     'chatapp',
