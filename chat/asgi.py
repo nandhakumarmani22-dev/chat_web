451c4b4455d3db32da5895247a0b900a7486317a
Nandha kuame <nandhakumarmani22@gmail.com>
2026-04-10 15:14:36 +0530

    agsi order set

--- chat/asgi.py
+++ chat/asgi.py
@@ -1,12 +1,13 @@
 import os
 import django
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import chatapp.routing 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
 django.setup()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chatapp.routing 
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
